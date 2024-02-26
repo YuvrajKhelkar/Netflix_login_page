f836bc48872ecc4a165e874a1e1a7a7a9a3bed66
YuvrajKhelkar <khelkaryuvraj@gmail.com>
2024-02-26 17:51:47 +0530

    Netflix

---
+++ n.js
@@ -0,0 +1,13 @@
+
+
+document.querySelector('.form').addEventListener('submit', (e) => {
+    e.preventDefault();
+   
+    console.log('Form submitted');
+  });
+  
+  document.querySelector('.signup-link').addEventListener('click', (e) => {
+    e.preventDefault();
+   
+    console.log('Sign-up link clicked');
+  });
